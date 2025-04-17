65a27cebf1809fdb0da08410147b6c0289c7a901
galit20 <69456102+galit20@users.noreply.github.com>
2025-04-17 13:57:59 +0300

    count vowels final

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -4,7 +4,8 @@ const stringToArray = R.split("");
 
 /* Question 1 */
 const vowels: string[] = ['a', 'e', 'i', 'o', 'u'];
-export const countVowels: undefined = undefined;
+export const countVowels: (x : string) => number = (x: string) : number => R.reduce((acc : number, cur : string) => vowels.includes(cur.toLowerCase()) ? ++acc : acc ,0, stringToArray(x));
+
 
 /* Question 2 */
 export const isPalindrome: undefined = undefined;
