659b65f20b818a370993199c8d5de7f321893215
galit20 <69456102+galit20@users.noreply.github.com>
2025-04-17 14:02:08 +0300

    isPalindrome final

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -8,7 +8,12 @@ export const countVowels: (x : string) => number = (x: string) : number => R.red
 
 
 /* Question 2 */
-export const isPalindrome: undefined = undefined;
+//every stops and immediately returns false at the moment it finds an element that does not satisfy the predicate
+export const isPalindrome : (x : string) => boolean = (x: string) : boolean => R.pipe(
+    (x : string) : string[] => stringToArray(x.toLowerCase()).filter(char => char >= '0' && char <= '9' || char >= 'a' && char <= 'z'),
+    (x : string[]) : string[][] => x.length % 2 === 0 ? [x.slice(0, x.length / 2), x.slice(x.length/ 2)] : [x.slice(0, x.length / 2), x.slice(x.length / 2 + 1)],
+    (x : string[][]) : boolean => x[0].every((char : string, index : number) => char === x[1][x[0].length - 1 - index])
+)(x);
   
 /* Question 3 */
 export type WordTree = {
